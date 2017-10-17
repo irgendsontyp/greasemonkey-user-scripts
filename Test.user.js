1914272840ca2026b9d94d79e2f6e281a20dba9b
irgendsontyp <htcmaxx@gmail.com>
2017-10-17 19:38:43 +0200

    Test script

---
+++ Test.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        Test
+// @namespace   https://github.com/irgendsontyp/greasemonley-user-scripts
+// @description Test only
+// @version     1
+// @grant       none
+// ==/UserScript==
